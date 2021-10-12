b173fdcf2b9cf90e59e972e529208f1ff2e98d81
YagoSilvaA <83378492+YagoSilvaA@users.noreply.github.com>
2021-10-11 22:52:18 -0500

    Delete backend directory

--- backend/restaurant.sql
+++
@@ -1,5 +0,0 @@
-CREATE database restaurant;
-use restaurant;
-CREATE TABLE menu (price int, meal varchar(255), created_date datetime, modified_date datetime);
-INSERT INTO menu (price, meal) VALUES (40, 'lomo saltado con papa nativa, no hay papa frita :c');
-INSERT INTO menu (price, meal) VALUES (23, 'causita rellena');
